d20d8f905a457c0339be9ed5bd50bb5c5e3539d8
gabriel <gabriel.lealsantos@hotmail.com>
2024-04-02 10:50:18 -0300

    Criação da IA generativa

--- src/main/java/me/dio/IALol/application/AskChampionsUseCase.java
+++ src/main/java/me/dio/IALol/application/AskChampionsUseCase.java
@@ -3,15 +3,18 @@ package me.dio.IALol.application;
 import me.dio.IALol.domain.exception.ChampionNotFoundException;
 import me.dio.IALol.domain.models.Champion;
 import me.dio.IALol.domain.ports.ChampionsRepository;
+import me.dio.IALol.domain.ports.GenerativeAiApi;
 
 import java.util.List;
 
-public record AskChampionsUseCase(ChampionsRepository repository) {
+public record AskChampionsUseCase(ChampionsRepository repository, GenerativeAiApi genAiApi) {
 
     public String askChampion(Long championId, String question ) {
 
         Champion champion = repository.findById(championId)
                 .orElseThrow(() -> new ChampionNotFoundException(championId));
+
+        String championContext = champion.generateContextByQuestion(question);
 
         return champion.generateContextByQuestion(question);
     }
---
+++ src/main/java/me/dio/IALol/domain/ports/GenerativeAiApi.java
@@ -0,0 +1,6 @@
+package me.dio.IALol.domain.ports;
+
+public interface GenerativeAiApi {
+
+    String generateContent(String objective, String context);
+}
